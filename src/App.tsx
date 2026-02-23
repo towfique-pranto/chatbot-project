4cbd0c81b397dd661d1a16701e21b3cee22d6a81
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-23 17:29:10 +0600

    Convert ChatMessage and ChatMessages components to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import robot from './assets/robot.png'
 import './App.css'
 
 function App() {
-  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || []);
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages') || '[]'));
   const title = `${chatMessages.length} Messages`
   useEffect(() => {
     Chatbot.addResponses({
--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -4,16 +4,16 @@ import UserProfileImage from '../assets/profile-1.jpg'
 import LoadingSpinner from "../assets/loading-spinner.gif";
 import './ChatMessage.css'
 
-
-export function ChatMessage({ message, sender, time}) {
+type ChatMessageProps = { message: string, sender: string, time: string }
+export function ChatMessage({ message, sender, time }: ChatMessageProps) {
 
   return (
     <div className={sender === "user" ? "user-message" : "robot-message"}>
       {sender === "robot" && (
         <img src={RobotProfileImage} className="chat-message-profile" />
       )}
-      <div className="chat-message-text">{message==='isLoadingGif'?<img className="loadingImg" src={LoadingSpinner} />:message} <p className="chat-message-time">{dayjs(time).format('h:mma')}</p></div>
-      
+      <div className="chat-message-text">{message === 'isLoadingGif' ? <img className="loadingImg" src={LoadingSpinner} /> : message} <p className="chat-message-time">{dayjs(time).format('h:mma')}</p></div>
+
       {sender === "user" && (
         <img src={UserProfileImage} className="chat-message-profile" />
       )}
