3531dfb39bbbd01924900089d21f27db08a53239
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-23 20:05:09 +0600

    deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/chatbot-project/",
   plugins: [react({
     babel: {
       plugins: [['babel-plugin-react-compiler', { target: '19' }]],
