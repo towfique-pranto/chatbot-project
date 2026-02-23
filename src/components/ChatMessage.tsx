3109668f726891aa6aa9d0b7d8fa7e1dcc33c2b4
Towfique Kabir <towfique.pranto@gmail.com>
2026-02-23 20:07:16 +0600

    update user profile image

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import RobotProfileImage from '../assets/robot.png'
-import UserProfileImage from '../assets/profile-1.jpg'
+import UserProfileImage from '../assets/user.png'
 import LoadingSpinner from "../assets/loading-spinner.gif";
 import './ChatMessage.css'
 
